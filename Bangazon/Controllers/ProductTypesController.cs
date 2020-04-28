d2a503c3a7df0390a03cc70f0e53dffcc15cbb9d
Daniel Bennett <danielbradleybennett@gmail.com>
2020-04-27 23:14:37 -0500

    productTypes

---
+++ Bangazon/Controllers/ProductTypesController.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Bangazon.Data;
+using Bangazon.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Bangazon.Models.ProductTypeViewModels;
+using System.Runtime.CompilerServices;
+
+namespace Bangazon.Controllers
+{
+    [Authorize]
+    public class ProductTypesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProductTypesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: ProductTypes
+        public async Task<ActionResult> Index()
+        {
+
+            var products = await _context.ProductType
+                .Include(p => p.Products)
+                .Select(pt => new ProductList()
+                {
+                    Id = pt.ProductTypeId,
+                    Name = pt.Label,
+                    ProductCount = pt.Products.Count(),
+                    Products = pt.Products.OrderByDescending(p => p.DateCreated).Take(3)
+                }).ToListAsync();
+
+
+            return View(products);
+        }
+
+        // GET: ProductTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _context.ProductType
+                .Include(pt => pt.Products)
+                .Select(pt => new ProductList()
+                {
+                    Id = pt.ProductTypeId,
+                    Name = pt.Label,
+                    ProductCount = pt.Products.Count(),
+                    Products = pt.Products.Where(p => p.ProductTypeId == id)
+                }).ToListAsync();
+
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+
+        }
+
+        // GET: ProductTypes/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductTypes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductTypeId,Label")] ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(productType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productType);
+
+        }
+
+        // GET: ProductTypes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductTypes/Edit/5
+
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _context.ProductType.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(productType);
+
+        }
+
+        // POST: ProductTypes/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id, [Bind("ProductType,Label")] ProductType productType)
+        {
+            if (id != productType.ProductTypeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(productType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductTypeExists(productType.ProductTypeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productType);
+        }
+
+
+
+        // GET: ProductTypes/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _context.ProductType
+                .FirstOrDefaultAsync(m => m.ProductTypeId == id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+
+        }
+
+        // POST: ProductTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            var productType = await _context.ProductType.FindAsync(id);
+            _context.ProductType.Remove(productType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+         private bool ProductTypeExists(int id)
+            {
+                return _context.ProductType.Any(e => e.ProductTypeId == id);
+            }
+        }
+
+    } 
---
+++ Bangazon/Migrations/20200428035727_update.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bangazon.Migrations
+{
+    public partial class update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Label",
+                table: "ProductType",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductListId",
+                table: "Product",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ProductList",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    ProductCount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductList", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "00000000-ffff-ffff-ffff-ffffffffffff",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "35db65d9-8db4-4b18-83b8-708a56e86d1b", "AQAAAAEAACcQAAAAEFbSfe3BvEpV42X7QjFWNRUuaHedT7Rk8/Q2wrkBDbOP0aZCKqbMbKlExaBnk4UcMw==" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_ProductListId",
+                table: "Product",
+                column: "ProductListId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Product_ProductList_ProductListId",
+                table: "Product",
+                column: "ProductListId",
+                principalTable: "ProductList",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Product_ProductList_ProductListId",
+                table: "Product");
+
+            migrationBuilder.DropTable(
+                name: "ProductList");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Product_ProductListId",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "ProductListId",
+                table: "Product");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Label",
+                table: "ProductType",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "00000000-ffff-ffff-ffff-ffffffffffff",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "d3a3f89f-1d18-41d1-84a6-989a7ae90dce", "AQAAAAEAACcQAAAAELPuCbLoTcpmFfuifZ9uzZsTpMZOm61V1gXuPG/P9ksZLR7bs8NuQf5122rGEzh7sA==" });
+        }
+    }
+}
