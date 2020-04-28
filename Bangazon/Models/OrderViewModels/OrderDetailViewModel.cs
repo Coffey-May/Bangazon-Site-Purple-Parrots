82daa32cb500ab4b4fc0b3d9f8c943769c0b1573
Taylor <taylorhcarroll1@gmail.com>
2020-04-27 19:42:17 -0700

    refactored view cart to match new methods

--- Bangazon/Models/OrderViewModels/OrderDetailViewModel.cs
+++ Bangazon/Models/OrderViewModels/OrderDetailViewModel.cs
@@ -5,11 +5,6 @@ namespace Bangazon.Models.OrderViewModels
 {
     public class OrderDetailViewModel
     {
-        //public OrderDetailViewModel(Order order)
-        //{
-        //    Order = order;
-        //    LineItems = new List<OrderLineItem>();
-        //}
         public Order Order { get; set; }
       
         public IEnumerable<OrderLineItem> LineItems { get; set; }
